4ed533674934be2fe351a1ce807dc36bbaa60a0b
Yu <ge75qeb@mytum.de>
2026-07-17 02:45:33 +0900

    fix: allocate bug fix and dca optimized

--- src/backtesting/dca.py
+++ src/backtesting/dca.py
@@ -11,27 +11,21 @@ def dca(prices, indicator, amount = 100, period_in_d=21, start_capital=1000, sta
   for days in range(start_date, len(prices)):
     if days % period_in_d == 0:
       not_invested_capital += amount
-      shares, not_invested_capital = allocate(shares, not_invested_capital, prices.iloc[days], indicator.iloc[days])
+      if indicator is None:
+        target = 1.0                        
+      else:
+        target = indicator.iloc[days]
+      if not pd.isna(target):
+        shares, not_invested_capital = allocate(shares, not_invested_capital, prices.iloc[days], target)
        
     portfolio_value.append(shares * prices.iloc[days])
   return pd.Series(portfolio_value,index=prices.index[start_date:])
  
-def allocate(shares, capital, price, day):
-  if day < 1:
-    shares *= math.floor(1 - day)
-    capital += price * shares
-    return shares, capital
-  if capital >= price:
-    shares = math.floor(capital / price)
-    capital -= shares * price
-    return shares, capital
-  return 0, capital
+def allocate(shares, capital, price, target):
+  potential = shares + math.floor(capital / price)
+  real_shares_amount = math.floor(potential * target)
+  capital += (shares - real_shares_amount) * price
+  return real_shares_amount, capital 
 
-
-def only_buy_draw_down(prices):
-  draw_down = False
-
-  return
-
-def only_buy(prices):
-  return pd.Series(1.0, index=range(len(prices)))
+ 
+ 
