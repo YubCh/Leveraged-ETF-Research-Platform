e1707105ca83b06892ff28065f8aa4774c6e8e17
Yu <ge75qeb@mytum.de>
2026-07-28 19:54:09 +0900

    fix: allocate was selling when price was rising and target stayed the same. max() prevents that

--- src/backtesting/dca.py
+++ src/backtesting/dca.py
@@ -6,8 +6,7 @@ import math
 def dca(prices, indicator=None, income = 100, income_period = 21, invest_period=21, start_capital=1000, start_date = 0):
   portfolio_value = []
   shares = 0
-  not_invested_capital = start_capital
-  
+  not_invested_capital = start_capital 
   for days in range(start_date, len(prices)):
     if days % income_period == 0:
       not_invested_capital += income
@@ -18,13 +17,13 @@ def dca(prices, indicator=None, income = 100, income_period = 21, invest_period=
         target = indicator.iloc[days]
       if not pd.isna(target):
         shares, not_invested_capital = allocate(shares, not_invested_capital, prices.iloc[days], target)
-       
     portfolio_value.append(shares * prices.iloc[days] + not_invested_capital)
   return pd.Series(portfolio_value,index=prices.index[start_date:])
  
 def allocate(shares, capital, price, target):
   potential = shares + math.floor(capital / price)
   real_shares_amount = math.floor(potential * target)
+  real_shares_amount = max(real_shares_amount, shares)
   capital += (shares - real_shares_amount) * price
   return real_shares_amount, capital 
    
