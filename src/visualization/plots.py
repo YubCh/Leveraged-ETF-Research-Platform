1e1a2396ba22b2317b9b443ee15efaa8c9304b22
Yu <ge75qeb@mytum.de>
2026-07-28 23:12:25 +0900

    feat: histogram drawing amount of dd in given timeline

--- src/visualization/plots.py
+++ src/visualization/plots.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
-
+import numpy as np
+from src.research.stats import count_draw_downs
 
 def compare(series_dict, title="", normalize=True, log=True):
     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 8), sharex=True,
@@ -12,7 +13,6 @@ def compare(series_dict, title="", normalize=True, log=True):
         ax1.plot(s.index, s, label=name)
         dd = s / s.cummax() - 1
         ax2.plot(dd.index, dd)
-
     if log:
         ax1.set_yscale("log")
     ax1.legend()
@@ -21,3 +21,22 @@ def compare(series_dict, title="", normalize=True, log=True):
     ax2.set_ylabel("drawdown")
     plt.tight_layout()
     plt.show()
+
+#ccounts the drawdown from start to end date if mean=True we get the mean of a single year compressed. 
+def draw_down_histogram(prices, start, end=None, mean=False):
+    if end is None:
+        end = len(prices)
+    dd = np.array(range(10,90,10))
+    total_days = end - start
+    scale_to_year =  365 / total_days
+    scale = scale_to_year if mean else 1 
+    count = np.array([])
+    for each in dd:
+        count = np.append(count, count_draw_downs(prices,start,each))
+
+    plt.bar(dd,count*scale,width=1.0,edgecolor='black',color='blue', align="edge")
+    plt.xlabel("Drawdown %")
+    plt.ylabel("Per year" if mean else "Count")
+    plt.title(f"Drawdowns frequency per a year" if mean else f"Amount of DrawDowns from year: {start} to {end}")
+    plt.show()
+
