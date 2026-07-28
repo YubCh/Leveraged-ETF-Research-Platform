a54032d8c5dc7d0cc69c76d51558bc07f5cdbc9c
Yu <ge75qeb@mytum.de>
2026-07-28 17:21:47 +0900

    feat: new dd function that allows to measure dd from the local peak

--- src/backtesting/strategy.py
+++ src/backtesting/strategy.py
@@ -10,6 +10,13 @@ def defined_drawdown(prices, max_at, min_at = 0, exponent=1):
   target[dd <= min_at] = float("nan")
   return target.round(4).shift(1)
 
+#this prevents us from investing the plain dca if we are years below the peak
+def defined_drawdown_window(prices, max_at,min_at=0,exponent=1,window=252):
+  peak = prices.rolling(window, min_periods=1).max()
+  dd= (1-prices/peak)*100
+  target = (dd/max_at).clip(upper=1.0) ** exponent
+  target[dd <= min_at] = float("nan")
+  return target.round(4).shift(1)
 
 #buy at top
 def buy_high(prices):
